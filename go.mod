b1d888d2bb411d0443618a0f1308c0860d330aca
12end <1747433021@qq.com>
2022-08-14 22:49:26 +0800

    add log support

--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/12end/pocRegistry
 
 go 1.18
 
-require github.com/12end/requests v0.0.0-20220814024128-5fab9cf698ee
+require (
+	github.com/12end/requests v0.0.0-20220814024128-5fab9cf698ee
+	go.uber.org/zap v1.22.0
+)
 
 require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/net v0.0.0-20220811182439-13a9a731de15 // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- registry.go
+++ registry.go
@@ -4,17 +4,44 @@ import (
 	"errors"
 	"fmt"
 	"github.com/12end/requests"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"net/url"
 	"strings"
 )
 
 type Registry struct {
 	// map[productName][vulName]
-	pocs map[string]map[string]POC
+	pocs     map[string]map[string]POC
+	logLevel *zap.AtomicLevel
+	Logger   *zap.Logger
 }
 
 func NewRegistry() Registry {
-	return Registry{pocs: map[string]map[string]POC{}}
+	level := zap.NewAtomicLevelAt(zap.ErrorLevel)
+	config := zap.Config{
+		Level:            level,
+		Encoding:         "console",
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
+		EncoderConfig: zapcore.EncoderConfig{
+			MessageKey:  "msg",
+			LevelKey:    "level",
+			TimeKey:     "time",
+			LineEnding:  zapcore.DefaultLineEnding,
+			EncodeLevel: zapcore.CapitalColorLevelEncoder,
+			EncodeTime:  zapcore.TimeEncoderOfLayout("15:04:05"),
+		},
+	}
+	log, err := config.Build()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return Registry{pocs: map[string]map[string]POC{}, logLevel: &level, Logger: log}
+}
+
+func (r Registry) SetLogLevel(level zapcore.Level) {
+	r.logLevel.SetLevel(level)
 }
 
 func (r Registry) Register(poc POC) {
